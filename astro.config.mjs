577a86c885ee4379912a78c0b9900eb5dc229504
Gerardo Fernandez Diaz <gfdiaz@bi.com>
2025-06-20 19:09:39 -0600

    Fix base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import robotsTxt from 'astro-robots-txt'
+import tailwind from "@astrojs/tailwind"
+import robotsTxt from "astro-robots-txt"
 
 export default defineConfig({
-  site: 'https://gfdiaz1121.github.io/my-portfolio/',
+  base: '/my-portfolio/', // <-- IMPORTANT FIX
+  site: 'https://gfdiaz1121.github.io/my-portfolio',
   integrations: [tailwind(), robotsTxt()],
-  outDir: './docs' // ⭐️ important!
 })
