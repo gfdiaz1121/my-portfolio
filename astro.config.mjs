4f1bbd5eacc508815c2c18efee45ed409913f76e
Gerardo Fernandez Diaz <gfdiaz@bi.com>
2025-06-20 19:34:45 -0600

    Fix base URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
 import robotsTxt from "astro-robots-txt"
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: 'https://gfdiaz1121.github.io/my-portfolio/',
+  base: '/my-portfolio/',
 })
