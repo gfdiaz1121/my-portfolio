886b2f4210f2bd4849d9a4c94e95b2ec94c8163b
Gerardo Fernandez Diaz <gfdiaz@bi.com>
2025-06-20 19:32:35 -0600

    Fix CSS loading and deploy paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
+
 import robotsTxt from "astro-robots-txt"
 
+// https://astro.build/config
 export default defineConfig({
-  base: '/my-portfolio/', // <-- IMPORTANT FIX
-  site: 'https://gfdiaz1121.github.io/my-portfolio',
   integrations: [tailwind(), robotsTxt()],
+  site: 'https://porfolio.dev/'
 })
